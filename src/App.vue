7a589544b99ad3d453ec5a48491645c8bc247286
carlomarcon <carlomarcon1998@gmail.com>
2024-02-21 16:48:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script>
+import axios from "axios";
+export default {};
+</script>
+<template></template>
+<style lang="scss">
+@use "./styles/general.scss" as *;
+body {
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,7 @@
+<script>
+import axios from "axios";
+</script>
+<template>
+  <router-view></router-view>
+</template>
+<script lang="scss"></script>
