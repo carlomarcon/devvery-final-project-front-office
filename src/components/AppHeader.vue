e81d519a3422856ea7416295a7c6a2c8d7ec4952
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-02-26 11:01:28 +0100

    fix: header syntax

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,37 +10,38 @@ export default {
 </script>
 
 <template >
-  <nav class="navbar fixed-top bg-body-tertiary">
-    <div class="container ">
-      <a class="navbar-brand" href="">
-        <img src="../img/logotipo.png" alt="" />
-      </a>
+  <header>
+    <nav class="navbar fixed-top bg-body-tertiary">
+      <div class="container ">
 
+        <a class="navbar-brand" href="#">
+          <img src="../img/logotipo.png" alt="" />
+        </a>
 
-      <div class="d-flex gap-3">
-        <a href="" class="btn d-none d-md-block d-lg-block ">CHI SIAMO</a>
-        <a href="http://127.0.0.1:8000/admin" class="btn btn-warning d-none d-md-block d-lg-block  ">LOGIN</a>
-        <div><button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        <div class="d-flex gap-3 align-items-center">
+          <a href="" class="btn d-none d-md-block fw-bold">CHI SIAMO</a>
+          <a href="" class="btn ms_btn-yellow d-none d-md-block">LOGIN</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
             aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <i class="fa-solid fa-cart-shopping"></i>
           </button>
-          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
-              <h2 class="offcanvas-title" id="offcanvasNavbarLabel">Il tuo carrello</h2>
-              <button type="button" class="btn-close ms_burger" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-
-            </div>
-
-            <div class="offcanvas-body">
-              <AppCart />
-            </div>
-          </div>
         </div>
-      </div>
 
+      </div>
+    </nav>
+  </header>
+
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div class="offcanvas-header">
+      <h2 class="offcanvas-title" id="offcanvasNavbarLabel">Il tuo carrello</h2>
+      <button type="button" class="btn-close ms_burger" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
 
-  </nav>
+    <div class="offcanvas-body">
+      <AppCart />
+    </div>
+  </div>
+  
 </template>
 
 
@@ -53,7 +54,7 @@ img {
 }
 
 nav {
-  height: 5rem; 
+  height: 5rem;
 
 }
 
