1ec2507e93aa410f2b923d2a79b4bbc2ee3d9e18
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2024-02-29 10:27:36 +0100

    improved cart graphics

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -34,39 +34,50 @@ export default {
       this.store.total = total;
       localStorage.setItem('total', JSON.stringify(this.store.total));
       return total;
+    },
+    emptyCart() {
+      this.store.cartData = [];
+      localStorage.clear();
     }
   },
 };
 </script> 
 
 <template>
-  <div>
-    <ul v-if="store.cartData.length > 0" class="list-group">
-      <li v-for="(item, index) in store.cartData" :key="index" class="list-group-item">
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="info">
-            <span class="d-inline-block mb-1 text-capitalize">{{ item.name }}</span> - <span>{{ item.price }}€</span><br>
+  <div class="wrapper">
+    <div v-if="store.cartData.length > 0">
+      <div class="text-end">
+        <button class="btn btn-outline-danger mb-3 btn-sm me-3" @click="emptyCart()">Svuota il carrello</button>
+      </div>
+      <ul class="list-group mb-4">
+        <li v-for="(item, index) in store.cartData" :key="index" class="list-group-item">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="info">
+              <span class="d-inline-block mb-1 text-capitalize">{{ item.name }}</span> - <span>{{ item.price
+              }}€</span><br>
 
-            <button class="px-2 py-1 bg-warning-subtle border-0 rounded-circle lh-1 fw-bold"
-              @click="decrease(item.id)">-</button>
-            <span class="d-inline-block mx-2">{{ item.quantity }}</span>
-            <button class="px-2 py-1 bg-warning-subtle border-0 rounded-circle lh-1 fw-bold"
-              @click="add(item.id)">+</button>
+              <button class="px-2 py-1 bg-warning-subtle border-0 rounded-circle lh-1 fw-bold"
+                @click="decrease(item.id)">-</button>
+              <span class="d-inline-block mx-2">{{ item.quantity }}</span>
+              <button class="px-2 py-1 bg-warning-subtle border-0 rounded-circle lh-1 fw-bold"
+                @click="add(item.id)">+</button>
+            </div>
+
+            <button class="btn btn-outline-danger btn-sm" @click="remove(item.id)">
+              <i class="fa-solid fa-trash"></i>
+            </button>
           </div>
+        </li>
+      </ul>
 
-          <button class="btn btn-outline-danger btn-sm" @click="remove(item.id)">
-            Rimuovi dal carrello
-          </button>
-        </div>
-      </li>
-    </ul>
-    <p v-else class="alert alert-light">Il carrello è vuoto</p>
-
-    <div v-if="store.cartData.length > 0" class="mt-4">
-      <p class="fw-bold">Totale: {{ calculateTotal() }}€</p>
-      <router-link v-if="store.cartData.length > 0" to="/shipment" class="btn btn-outline-success">Vai alla
-        spedizione</router-link>
+      <div class="d-flex justify-content-between align-items-baseline mx-3">
+        <h4 class="fw-bold">Totale: {{ calculateTotal() }}€</h4>
+        <router-link v-if="store.cartData.length > 0" to="/shipment" class="btn btn-outline-success">Vai alla
+          spedizione</router-link>
+      </div>
     </div>
+
+    <p v-else class="alert alert-light mx-4">Il carrello è vuoto</p>
   </div>
 </template>
 
