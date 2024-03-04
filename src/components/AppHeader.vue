a982ac00e06342cb311bc6a3aacb05ddbcaf3eb9
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2024-03-04 10:58:33 +0100

    fixed cart badge

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -36,11 +36,14 @@ export default {
         <div v-if="$route.path != '/shipment'" class="d-flex gap-2 align-items-center">
           <router-link class="btn d-none d-md-block fw-bold" to="/about-us">CHI SIAMO</router-link>
           <a href="http://127.0.0.1:8000/admin" class="btn ms_btn-yellow d-block">LOGIN</a>
-          <button class="navbar-toggler border-black border-2 d-flex align-content-center gap-1" type="button"
+          <button class="navbar-toggler border-black border-2 d-flex align-content-center gap-1 position-relative" type="button"
             data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
             aria-label="Toggle navigation" @click="closeModal()">
-            <span v-if="store.cartData.length != 0" class="d-inline-block ms_color-dark fw-bold">{{ numberOfProducts
-              }}</span>
+
+            <!-- <span v-if="store.cartData.length != 0" class="d-inline-block ms_color-dark fw-bold">{{ numberOfProducts }}</span> -->
+
+              <span v-if="store.cartData.length != 0" class="counter d-inline-block ms_color-dark fw-bold rounded-2 position-absolute top-0 start-100 translate-middle">{{ numberOfProducts }}</span>
+
             <i class="fa-solid fa-cart-shopping ms_color-dark"></i>
           </button>
         </div>
@@ -74,6 +77,15 @@ img {
 
 i {
   font-size: 1.5rem;
+}
+
+.counter {
+  background-color: #f9b91f;
+  padding: 0.2rem 0.4rem;
+}
+
+button:hover {
+  background-color: rgba(199, 199, 199, 0.3);
 }
 
 .offcanvas-header {
